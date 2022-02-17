6002e5fdc8bff0a927b4a21f9acdf6a07084d014
Ander Gray <ander.gray@liverpool.ac.uk>
2022-02-17 20:14:53 +0000

    creates module

--- src/UncLogic.jl
+++ src/UncLogic.jl
@@ -1,6 +1,6 @@
 ###
 #
-#       Part of the UncLogic.jl package, for pushing uncertainty through 
+#       Part of the UncLogic.jl package, for pushing uncertainty through
 #       logical statements
 #
 #       This file defines the package
@@ -16,59 +16,69 @@
 #                    -> Fix correlated sampler (close)
 #                    -> Fix and: use Scotts model with checking of correlation coefficient
 #                    -> Compute joint distributions with correlations and 2 marginals
-#                    -> 
+#                    ->
 #
 #
 #
 #
 ###
 
-using ProbabilityBoundsAnalysis, IntervalArithmetic, Distributions, StatsBase, LinearAlgebra
+module UncLogic
 
-import Base: ~, !, <, >,==
+    using ProbabilityBoundsAnalysis, IntervalArithmetic, Distributions, StatsBase, LinearAlgebra, PossibilisticArithmetic
 
-import ProbabilityBoundsAnalysis.isscalar
+    import Base: ~, !, <, >,==
 
-include("../../FuzzyArithmetic.jl/src/FuzzyNumbers.jl")
+    import ProbabilityBoundsAnalysis.isscalar
 
-UncBool = Union{Bool, Int64, Float64, <:AbstractInterval, <: AbstractPbox, <: AbstractFuzzy}
+    #UncBool = Union{Bool, Int64, Float64, <:AbstractInterval, <: AbstractPbox, <: AbstractFuzzy}
 
-#global DefaultCorr = interval(-1,1);
-global DefaultCorr = 0;
+    UncBool = Union{Bool, Int64, Float64, <:AbstractInterval, <: AbstractPbox}
 
-dual(x :: Interval) = Interval(x.hi,x.lo)
-dual(x :: Real) = x
+    #global DefaultCorr = interval(-1,1);
+    global DefaultCorr = 0;
 
-right(x :: pbox) = x.d[end]
-left(x :: pbox) = x.u[1]
+    dual(x :: Interval) = Interval(x.hi,x.lo)
+    dual(x :: Real) = x
 
-isDual(x :: Interval) = x.hi < x.lo
-isDual(x :: Real) = false
+    right(x :: pbox) = x.d[end]
+    left(x :: pbox) = x.u[1]
 
-isequal(x :: pbox, y ::Union{Int64, Float64}) = false
-isequal(y ::Union{Int64, Float64}, x :: pbox) = isequal(x, y)
+    isDual(x :: Interval) = x.hi < x.lo
+    isDual(x :: Real) = false
 
-isequal(x :: FuzzyNumber, y ::Union{Int64, Float64}) = false
-isequal(y ::Union{Int64, Float64}, x :: FuzzyNumber) = isequal(x, y)
+    isequal(x :: pbox, y ::Union{Int64, Float64}) = false
+    isequal(y ::Union{Int64, Float64}, x :: pbox) = isequal(x, y)
 
-isscalar(x:: FuzzyNumber) = false
+    isequal(x :: FuzzyNumber, y ::Union{Int64, Float64}) = false
+    isequal(y ::Union{Int64, Float64}, x :: FuzzyNumber) = isequal(x, y)
 
-==(x :: pbox,y ::Union{Int64, Float64}) = isequal(x,y)
-==(y ::Union{Int64, Float64}, x :: pbox) = isequal(y,x)
+    isscalar(x:: FuzzyNumber) = false
 
-==(x :: FuzzyNumber,y ::Union{Int64, Float64}) = isequal(x,y)
-==(y ::Union{Int64, Float64}, x :: FuzzyNumber) = isequal(y,x)
+    ==(x :: pbox,y ::Union{Int64, Float64}) = isequal(x,y)
+    ==(y ::Union{Int64, Float64}, x :: pbox) = isequal(y,x)
 
-isless(x :: pbox, y :: Union{Int64, Float64}) = cdf(x,y);
-isless(y :: Union{Int64, Float64},x :: pbox) = cdf(complement(x),y);
+    ==(x :: FuzzyNumber,y ::Union{Int64, Float64}) = isequal(x,y)
+    ==(y ::Union{Int64, Float64}, x :: FuzzyNumber) = isequal(y,x)
 
-<(x :: pbox, y :: Union{Int64, Float64}) = isless(x,y)
-<(x :: Union{Int64, Float64}, y :: pbox) = isless(x,y)
+    isless(x :: pbox, y :: Union{Int64, Float64}) = cdf(x,y);
+    isless(y :: Union{Int64, Float64},x :: pbox) = cdf(complement(x),y);
 
->(x :: pbox, y :: Union{Int64, Float64}) = isless(y,x)
->(x :: Union{Int64, Float64}, y :: pbox) = isless(y,x)
+    <(x :: pbox, y :: Union{Int64, Float64}) = isless(x,y)
+    <(x :: Union{Int64, Float64}, y :: pbox) = isless(x,y)
 
-include("Checks.jl")
-include("Operations.jl")
-include("Dependence.jl")
-include("LogicSampler.jl")
+    >(x :: pbox, y :: Union{Int64, Float64}) = isless(y,x)
+    >(x :: Union{Int64, Float64}, y :: pbox) = isless(y,x)
+
+
+    export
+        and, not, or, ~, !, <, >, ==, isequal, nor, conditional, Joint, UncCorr, getValidCorr,
+        randomBool, corrBool, UncBool, checkUncBool, checkCor
+
+    include("Checks.jl")
+    include("Operations.jl")
+    include("Dependence.jl")
+    include("LogicSampler.jl")
+
+
+end # module
